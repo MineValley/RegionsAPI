814517f6ac8a8ef1dc2f4f0ae1cb410e806c442c
Vincent B. <79211348+Snabeldier@users.noreply.github.com>
2025-06-09 21:23:45 +0200

    feat: implement all core methods that include regions features (#3)

--- src/main/java/minevalley/regions/api/RegionsServer.java
+++ src/main/java/minevalley/regions/api/RegionsServer.java
@@ -2,24 +2,113 @@ package minevalley.regions.api;
 
 import minevalley.core.api.Registrant;
 import minevalley.core.api.corporations.RealEstateGroup;
+import minevalley.regions.api.core.Area;
+import minevalley.regions.api.core.Region;
+import minevalley.regions.api.residences.Apartment;
 import minevalley.regions.api.residences.ApartmentBlock;
+import minevalley.regions.api.residences.Plot;
 import minevalley.regions.api.residences.Residence;
+import minevalley.regions.api.structures.District;
+import minevalley.regions.api.structures.RadioMast;
+import minevalley.regions.api.structures.Street;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.jetbrains.annotations.ApiStatus;
+import org.jetbrains.annotations.Contract;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.stream.Stream;
 
 @ApiStatus.Internal
 public interface RegionsServer {
 
+    @Nullable
+    @Contract(pure = true)
+    Region getRegion(int id);
+
+    @Nonnull
+    @Contract(pure = true)
+    List<Region> getRegions(@Nonnull Block block) throws IllegalArgumentException;
+
+    @Nonnull
+    @Contract("_, _ -> new")
+    Region createRegion(@Nonnull List<Area> included, @Nonnull List<Area> excluded) throws IllegalArgumentException;
+
+    @Nullable
+    @Contract(pure = true)
     Residence getResidence(int id);
 
+    @Nullable
+    @Contract("null -> null")
+    Residence getResidence(@Nullable Region region);
+
+    @Nonnull
+    Stream<Residence> getResidences(@Nonnull Block block) throws IllegalArgumentException;
+
+    @Nonnull
+    @Contract(pure = true)
+    List<Residence> getResidences(@Nonnull Registrant registrant) throws IllegalArgumentException;
+
+    @Nonnull
+    @Contract(pure = true)
     List<Residence> getResidences();
 
-    List<Residence> getResidences(Registrant registrant);
+    @Nonnull
+    @Contract(pure = true)
+    List<Residence> getLoadedResidences();
 
-    ApartmentBlock getApartmentBlock(int id);
+    @Nullable
+    @Contract(pure = true)
+    Plot getPlot(int id);
 
+    @Nullable
+    @Contract(pure = true)
+    Apartment getApartment(int id);
+
+    @Nonnull
+    @Contract(pure = true)
     List<ApartmentBlock> getApartmentBlocks();
 
-    List<ApartmentBlock> getApartmentBlocks(RealEstateGroup realEstateGroup);
+    @Nonnull
+    @Contract(pure = true)
+    List<ApartmentBlock> getApartmentBlocks(@Nonnull RealEstateGroup realEstateGroup)
+            throws IllegalArgumentException;
+
+    @Nullable
+    @Contract(pure = true)
+    ApartmentBlock getApartmentBlock(int id);
+
+    @Nonnull
+    @Contract(pure = true)
+    List<Street> getStreets();
+
+    @Nullable
+    @Contract(pure = true)
+    Street getStreet(int id);
+
+    @Nonnull
+    @Contract(pure = true)
+    List<District> getDistricts();
+
+    @Nullable
+    @Contract(pure = true)
+    District getDistrict(int id);
+
+    @Nullable
+    @Contract("null -> null")
+    District getDistrict(@Nullable Chunk chunk);
+
+    @Nullable
+    @Contract("null -> null")
+    RadioMast getNearestRadioMast(@Nullable Location location);
+
+    @Nonnull
+    @Contract("_, _ -> new")
+    Area getArea(@Nonnull Block loc1, @Nonnull Block loc2) throws IllegalArgumentException;
+
+    void loadPreset(@Nonnull Area presetArea, @Nonnull Block presetPivot, @Nonnull Block mainWorldPivot)
+            throws IllegalArgumentException, IllegalStateException;
 }
