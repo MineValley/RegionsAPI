8d103b61d99dadf8b7556a36edfb0b4ae3d2f9fb
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-06-28 20:17:25 +0200

    chore: improve areas

--- src/main/java/minevalley/regions/api/core/Area.java
+++ src/main/java/minevalley/regions/api/core/Area.java
@@ -1,12 +1,13 @@
 package minevalley.regions.api.core;
 
+import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.util.stream.Stream;
+import java.util.List;
 
 @SuppressWarnings("unused")
 public interface Area {
@@ -17,6 +18,7 @@ public interface Area {
      * @return the block with the smallest x, y, and z coordinates in the area
      */
     @Nonnull
+    @Contract(pure = true)
     Block getMinBlock();
 
     /**
@@ -25,18 +27,29 @@ public interface Area {
      * @return the block with the highest x, y, and z coordinates in the area
      */
     @Nonnull
+    @Contract(pure = true)
     Block getMaxBlock();
 
     /**
-     * Gets a stream of all the blocks contained by this area.
+     * Gets a list of all the blocks contained by this area.
      * <p>
      * <b>Note:</b> This method consumes quiet a lot of resources. Refrain from using it, when other methods work fine as well.
      *
-     * @return stream of all blocks contained by this area.
+     * @return list of all blocks contained by this area.
      * @see #contains(Block)
      */
     @Nonnull
-    Stream<Block> getBlocks();
+    @Contract(pure = true)
+    List<Block> getBlocks();
+
+    /**
+     * Gets a list of all the chunks, this area lies in.
+     *
+     * @return list of all the chunks this area lies in.
+     */
+    @Nonnull
+    @Contract(pure = true)
+    List<Chunk> getChunks();
 
     /**
      * Checks whether the given block is contained by the area.
@@ -44,7 +57,7 @@ public interface Area {
      * @param block block to check (might be null)
      * @return true, if the block is contained by the area.
      */
-    @Contract("null -> false")
+    @Contract(value = "null -> false", pure = true)
     boolean contains(@Nullable Block block);
 
     /**
@@ -53,7 +66,7 @@ public interface Area {
      * @param location location to check (might be null)
      * @return true, is the location is contained by the area.
      */
-    @Contract("null -> false")
+    @Contract(value = "null -> false", pure = true)
     default boolean contains(@Nullable Location location) {
         if (location == null) return false;
         return contains(location.getBlock());
