726b6c8b1d46aadfbfebbe32961d77e1b0959b7e
Vincent B. <79211348+Snabeldier@users.noreply.github.com>
2025-07-18 19:42:13 +0200

    chore: add some useful methods (#18)

--- src/main/java/minevalley/regions/api/structures/RadioMast.java
+++ src/main/java/minevalley/regions/api/structures/RadioMast.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
@@ -58,13 +59,61 @@ public interface RadioMast {
 
     /**
      * Calculates the distance between a given location and this radio mast.
+     * <p>
+     * It is calculated in two dimensions (the y coordinate is neglected) and is rounded to full blocks.
      *
      * @param location location to calculate the distance to
      * @return distance as integer
      * @throws IllegalArgumentException if location is null or not in the same world as this radio mast.
+     * @see #isInRange(Location)
+     */
+    @Nonnegative
+    @Contract(pure = true)
+    int distance(@Nonnull Location location) throws IllegalArgumentException;
+
+    /**
+     * Calculates the distance between the given coordinates and this radio mast, rounded in full blocks.
+     *
+     * @param x x coordinate to get the distance to
+     * @param z z coordinate to get the distance to
+     * @return distance as integer
+     * @see #isInRange(int, int)
+     */
+    @Nonnegative
+    @Contract(pure = true)
+    int distance(int x, int z);
+
+    /**
+     * Gets whether a specific location is in range of this radio mast.
+     *
+     * @param location location to check
+     * @return true, if the given location is in the same world as the radio mast and lies inside the radio mast's range
+     * @see #range()
+     * @see #distance(Location)
+     */
+    @Contract(value = "null -> false", pure = true)
+    boolean isInRange(@Nullable Location location);
+
+    /**
+     * Gets whether specific coordinates lie in the range of this radio mast.
+     *
+     * @param x x coordinate to check
+     * @param z z coordinate to check
+     * @return true, if the given location lies inside the radio mast's range
+     * @see #range()
+     * @see #distance(int, int)
      */
     @Contract(pure = true)
-    double distance(@Nonnull Location location) throws IllegalArgumentException;
+    boolean isInRange(int x, int z);
+
+    /**
+     * Gets a stream of all the users that are in range of this radio mast.
+     *
+     * @return stream of all the users in range
+     */
+    @Nonnull
+    @Contract(pure = true)
+    Stream<OnlineUser> getOnlineUsersInRange();
 
     /**
      * Gets all the users that are currently connected to this radio mast.
